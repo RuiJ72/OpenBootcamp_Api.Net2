bd113665fc681fb8abda070293e644077176009b
RuiJ72 <rlagos900@gmail.com>
2022-09-05 00:06:22 -0300

    Project replication, and creation of  Chapters Controller

--- universityApiBackend/DataAccess/UniversityDBContext.cs
+++ universityApiBackend/DataAccess/UniversityDBContext.cs
@@ -13,5 +13,11 @@ namespace universityApiBackend.DataAccess
 
         // TODO: Add DbSets (The tables of data base)
         public DbSet<User>? Users { get; set; }
+        public DbSet<Course>? Courses { get; set; }
+        public DbSet<Chapter>? Chapters { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Student> Students { get; set; }
+        
+
     }
 }
---
+++ universityApiBackend/Models/DataModels/Student.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace universityApiBackend.Models.DataModels
+{
+    public class Student : BaseEntity
+    {
+        [Required]
+        public string FirstName { get; set; } = string.Empty;
+        
+        [Required]
+        public string LastName { get; set; } = string.Empty;
+        
+        [Required] // Date Of Birth
+        public DateTime DOB { get; set; }
+
+        public ICollection<Course> Courses { get; set; } = new List<Course>();
+    }
+}
